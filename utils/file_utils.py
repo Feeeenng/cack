385f47373b601fa022690f91a3064940a0e12ab0
haner27 <369685930@qq.com>
2017-02-17 17:58:10 +0800

    开始写图片上传

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -18,11 +18,11 @@ def type_list():
 # 字节码转16进制字符串
 def bytes2hex(bytes):
     num = len(bytes)
-    hexstr = u""
+    hexstr = u''
     for i in range(num):
-        t = u"%x" % bytes[i]
+        t = u'%x' % bytes[i]
         if len(t) % 2:
-            hexstr += u"0"
+            hexstr += u'0'
         hexstr += t
     return hexstr.upper()
 
@@ -34,7 +34,7 @@ def file_type(f):
     for hcode in tl.keys():
         numOfBytes = len(hcode) / 2  # 需要读多少字节
         f.seek(0)  # 每次读取都要回到文件头，不然会一直往后读取
-        hbytes = struct.unpack_from("B"*numOfBytes, f.read(numOfBytes)) # 一个 "B"表示一个字节
+        hbytes = struct.unpack_from('B' * numOfBytes, f.read(numOfBytes))  # 一个 'B'表示一个字节
         f_hcode = bytes2hex(hbytes)
         if f_hcode == hcode:
             ftype = tl[hcode]
